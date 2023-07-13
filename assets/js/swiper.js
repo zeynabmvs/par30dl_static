4aa9b635a40213f19c35990b5fc4a4bcb6342ed5
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2023-07-13 10:39:10 +0330

    WIP, working on swiper for tabs, failed

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,48 +1,62 @@
 // breakpoint where swiper will be destroyed
 // and switches to a dual-column layout
-const breakpoint = window.matchMedia( '(min-width:768px)' );
+const breakpoint = window.matchMedia('(min-width:768px)');
 // keep track of swiper instances to destroy later
 let mySwiper;
 //////////////////////////////////////////////////////////////////
-const breakpointChecker = function() {
-   // if larger viewport and multi-row layout needed
-   if ( breakpoint.matches === true ) {
-      // clean up old instances and inline styles when available
-      if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
-      // or/and do nothing
-      return;
-   // else if a small viewport and single column layout needed
-   } else if ( breakpoint.matches === false ) {
-      // fire small viewport version of swiper
-      return enableSwiper();
-   }
+const breakpointChecker = function () {
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+        // clean up old instances and inline styles when available
+        if (mySwiper !== undefined){
+         mySwiper.destroy(true, true);
+        }
+        
+        if (swiperNews !== undefined){
+         swiperNews.destroy(true, true);
+        }
+        if (swiperTabs !== undefined){
+         swiperTabs.destroy(true, true);
+        }
+
+        // also remove any swiper classes from DOM
+        // removeSwiperFromDom();
+
+        // or/and do nothing
+        return;
+        // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+        // fire small viewport version of swiper
+        return enableSwiper();
+    }
 };
 //////////////////////////////////////////////////////////////////
-const enableSwiper = function() {
-   mySwiper = new Swiper('#swiper-tiles', {
-    slidesPerView: 'auto',
-    spaceBetween: 16,
-    keyboardControl: true,
-  
-    breakpoints: {
-      // when window width is >= 768px
-      768: {
-        enabled: false
-      },
-  
-  }});
+const enableSwiper = function () {
+    mySwiper = new Swiper('#swiper-tiles', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        keyboardControl: true,
+    });
 
-  swiperNews = new Swiper('#swiper-news', {
-    slidesPerView: 'auto',
-    spaceBetween: 24,
-    pagination: {
-      // el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+    swiperNews = new Swiper('#swiper-news', {
+        slidesPerView: 'auto',
+        spaceBetween: 24,
+    });
 
-  
+    swiperTabs = new Swiper('#tabs-swiper', {
+        slidesPerView: 'auto',
+        spaceBetween: 16,
 
+      });
+
+
+}
+
+const removeSwiperFromDom = function () {
+    const swiperElements = document.querySelectorAll('.swiper, .swiper-wrapper, .swiper-slide');
+    swiperElements.forEach(element => {
+      element.classList.remove('swiper', 'swiper-wrapper', 'swiper-slide');
+    });
 }
 
 //////////////////////////////////////////////////////////////////
